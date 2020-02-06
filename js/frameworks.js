de704eeb3f527fa8e297c1047e04318cd5ab1472
John Slipper <john@notedemr.com>
2020-02-06 14:36:02 +1300

    Trimmed down to barebones Jekyll: - Removed all bower related code. - Removed codekit related code. - Removed all theme styling. - Removed all theme script files. - Updated Jekyll and config to latest.

--- js/frameworks.js
+++
@@ -1,3 +0,0 @@
-//@codekit-prepend("../bower_components/jquery/dist/jquery.js");
-//@codekit-prepend("../bower_components/foundation/js/foundation.js");
-//@codekit-prepend("../bower_components/fastclick/lib/fastClick.js");
